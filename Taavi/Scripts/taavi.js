4c2ac3633cc6f13a8eb9bcfbba9d486d17802504
Taavi Raudkiv <taaviraudkivi2@gmail.com>
2020-11-28 15:45:25 +0200

    New text reveal

--- Taavi/Scripts/taavi.js
+++ Taavi/Scripts/taavi.js
@@ -3,6 +3,13 @@ function toggleMenu(){
     nav.classList.toggle("active");
 }
 
+let progress = document.getElementById("progressbar");
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function() {
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+}
+
 new hoverEffect({
     parent: document.querySelector(".my-image"),
     intensity: 0.5,
