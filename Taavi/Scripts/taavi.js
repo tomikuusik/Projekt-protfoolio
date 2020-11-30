5a5bfbc45640be39c13e4405cad08be4c99479fe
Taavi Raudkiv <taaviraudkivi2@gmail.com>
2020-11-30 22:43:38 +0200

    Taavi final portfolio page

--- Taavi/Scripts/taavi.js
+++ Taavi/Scripts/taavi.js
@@ -1,14 +1,11 @@
-new hoverEffect({
-    parent: document.querySelector(".my-image"),
-    intensity: 0.5,
-    image1: "Pictures/124192624_1826986344121385_6771965711143217214_o.jpg",
-    image2: "Pictures/Outdoors-man-portrait_(cropped).jpg",
-    displacementImage: "Pictures/fluid.jpg"
-})
-
 function toggleMenu(){
     const nav = document.getElementById("hamburger-menu");
     nav.classList.toggle("active");
+}
+
+function closeMenu(){
+    const toggle = document.getElementsByClassName("menu-item");
+    toggle.classList.toggle("close");
 }
 
 let progress = document.getElementById("progressbar");
@@ -17,5 +14,3 @@ window.onscroll = function() {
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progress.style.height = progressHeight + "%";
 }
-
-new function ();
