27e92ab2dcad395e64791883dc9156ae3b26940a
Taavi Raudkiv <taaviraudkivi2@gmail.com>
2020-11-30 22:51:24 +0200

    Added comments

--- Taavi/Scripts/taavi.js
+++ Taavi/Scripts/taavi.js
@@ -1,13 +1,10 @@
+//Toggles CSS class "active" on click --> to open/close menu
 function toggleMenu(){
     const nav = document.getElementById("hamburger-menu");
     nav.classList.toggle("active");
 }
 
-function closeMenu(){
-    const toggle = document.getElementsByClassName("menu-item");
-    toggle.classList.toggle("close");
-}
-
+//https://www.youtube.com/watch?v=qYFkQa0Bbq0
 let progress = document.getElementById("progressbar");
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 window.onscroll = function() {
